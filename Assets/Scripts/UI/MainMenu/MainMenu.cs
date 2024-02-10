dec5277e499834cb9d552ff17e394482e8e93eb7
Megafont <51346737+Megafont@users.noreply.github.com>
2024-02-10 12:54:56 -0600

    Made minor requested changes

--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -2,36 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour
 {
-    [Tooltip("Just drag a scene into this field, and it will be loaded when the user clicks the Play button.")]
-    [SerializeField] private SceneAsset _SceneToLoadOnPlay;
+    [Tooltip("The name of the scene to load when the user clicks on the Play button.")]
+    [SerializeField] private string _sceneToLoadOnPlay;
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnPlayClicked()
     {
-        if (_SceneToLoadOnPlay != null)
-            SceneManager.LoadScene(_SceneToLoadOnPlay.name);
+        if (_sceneToLoadOnPlay != null)
+        {
+            SceneManager.LoadScene(_sceneToLoadOnPlay);
+        }
         else
+        {
             Debug.LogError("No scene has been specified to load when the play button is clicked!");
+        }
     }
 
     public void OnSettingsClicked()
