b0f530d984e88b9920a1f6ff4c659e857fffacbc
Megafont <51346737+Megafont@users.noreply.github.com>
2024-02-13 18:07:43 -0600

    A few tweaks and finished the task

--- Assets/Scripts/SaveData/DefaultSettingsMenuValues.cs
+++ Assets/Scripts/SaveData/DefaultSettingsMenuValues.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/// <summary>
+/// This class is just a storage place for all the default values of the options in the settings menu.
+/// That way these constants are all in the same place and easily accessible.
+/// </summary>
 public static class DefaultSettingsMenuValues
 {
     // AUDIO SETTINGS KEYS
--- Assets/Scripts/SaveData/PlayPrefsKeys.cs
+++ Assets/Scripts/SaveData/PlayPrefsKeys.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/// <summary>
+/// This class is just a storage place for all of the player prefs keys of the options in the settings menu.
+/// That way these constants are all in once place and easily accessible.
+/// </summary>
+
 public static class PlayerPrefsKeys
 {
 
