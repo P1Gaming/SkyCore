b0b624b68d087a6dda72b2d7896f48c098b8f230
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2024-02-19 13:01:35 -0500

    Lerp and Distance Checker

--- Assets/Scripts/Item/Generator/GeneratorHandler.cs
+++ Assets/Scripts/Item/Generator/GeneratorHandler.cs
@@ -3,44 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Threading;
 
 public class GeneratorHandler : MonoBehaviour
 {
     private float maxCharge = 100.0f;
     private float currentCharge = 0.0f;
+    private float targetCharge;
     [SerializeField] private Slider displaySlider;
     [SerializeField] private TMP_Text textDisplay;
 
-    [ContextMenu("DebugCharge")]
-    public void DebugCharge()
+    //Use these methods to increase the generator charge
+    public void AddCharge(float setCharge)
     {
-        SetCharge(50.0f);
+        targetCharge += setCharge;
     }
 
     public void SetCharge(float setCharge)
     {
-        currentCharge = setCharge;
-        SetTextBar();
-        SetPercentageBar();
+        targetCharge = setCharge;
     }
 
+    //Changes the percentage of the text
     private void SetTextBar()
     {
         textDisplay.text = GetPercentageText();
     }
-
+    //Changes the bar percentage
     private void SetPercentageBar()
     {
         displaySlider.value = GetPercentageFloat();
     }
-
+    //Calculates what text to display
     private string GetPercentageText()
     {
         return ((currentCharge / maxCharge) * 100).ToString("F0") + "%";
     }
-
+    //Calculates what percentage to display (ranges between 0 and 1)
     private float GetPercentageFloat()
     {
         return currentCharge / maxCharge;
+    }
+
+    //Lerp Bar Animation
+    private void FixedUpdate()
+    {
+        currentCharge = Mathf.Lerp(currentCharge, targetCharge, 0.9f*Time.deltaTime);
+        SetTextBar();
+        SetPercentageBar();
     }
 }
