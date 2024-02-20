8d3270ac7f57aa5ed31a0ab45ce89e36d08c2587
Robert Chaney <rwchaneyjr@yahoo.com>
2024-02-20 13:14:08 -0600

    Robert Chaney
    a cut scene with 3 sprites

--- Assets/Scenes/Move.cs
+++ Assets/Scenes/Move.cs
@@ -5,38 +5,109 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class Move : MonoBehaviour
 {
-    float speed = 35.0f;
+
+    public Canvas canvas;
+    int index;
+    float speed = 12f;
+    float speed1 =12f;
+    float speed2 =12f;
+    float time = 30f;
+   
     // Start is called before the first frame update
-    public Texture[] Image;
+    public Image[] ImageA;
     public Sprite[] S;
     public GameObject Wolf;
     public GameObject Pistol;
+    public GameObject Wolf2;
     bool _one = true;
-    bool _two = false;
+    bool _two = true;
+    bool _three = true;
+    bool _four = true;
     void Start()
     {
-        for (int i = 0; i < S.Length; i++)
+       
+    }
+    public void moveNext()
+    {
+        if (time <= 30 && _four)
+        {
+            ImageA[0].enabled = false;
 
-            S[i] = Resources.LoadAll<Sprite>("Images")[i];
+            _four = false;
 
+            ImageA[1].enabled = false;
+            ImageA[2].enabled = false;  
+        }
+        time -= Time.deltaTime; 
+    }
+    public void moveNext1()
+    {
+        if (time <= 25 && _two)
+        {
+            ImageA[0].enabled = true;
+
+            _two = false;
+
+            ImageA[1].enabled = false;
+            ImageA[2].enabled = false;
+
+            
+
+            //  yield return new WaitForSeconds(3f);
+        }
+        speed -= Time.deltaTime;
+    }
+    public void moveNext2()
+    {
+        if (time <= 20 && _one)
+        {
+
+            _one = false;
+            ImageA[1].enabled = true;
+            ImageA[0].enabled = false;
+            ImageA[2].enabled = false;
+            
+            //  yield return new WaitForSeconds(3f);
+        }
+        time -= Time.deltaTime;
+
+    }
+
+
+
+    public void moveNext3()
+
+    {
+
+        if (time <= 15 && _three)
+        {
+            ImageA[2].enabled = true;
+            ImageA[0].enabled = false;
+            ImageA[1].enabled = false;
+            // 
+        }
+        time -= Time.deltaTime;
+    }
+    public void moveNext4()
+
+    {
+
+        if (time <= 10 && _three)
+        {
+            ImageA[2].enabled = false;
+            SceneManager.LoadScene("Main"); 
+        }
+        
     }
     // Update is called once per frame
     void Update()
-    {speed -=Time.deltaTime;
-        if (speed < 15)
-        {
-           SceneManager.LoadScene("Main");
-        }       
-        transform.position += Vector3.right * 2f * Time.deltaTime;
-        if (transform.position.x > 10)
-        {
-            transform.position = new Vector3(-1, 0, 0);
-        }
-        Pistol.transform.position += Vector3.right * 2f * Time.deltaTime;
-        if (Pistol.transform.position.x > 10)
-        {
-            Pistol.transform.position = new Vector3(-1, 0, 0);
-        }
+    {   moveNext();
+        moveNext1();
+        moveNext2();
+        moveNext3();
+        moveNext4();
 
     }
+       
 }
+    
