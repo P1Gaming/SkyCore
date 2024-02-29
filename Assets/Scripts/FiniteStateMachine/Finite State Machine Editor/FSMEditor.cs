50aa0af4a24edcee81c37b95ece70d5ea5efe809
CaseyWalshWarder <111548933+CaseyWalshWarder@users.noreply.github.com>
2024-02-29 17:28:16 -0500

    Fix FSM editor not deleting removed parameter scriptable object

--- Assets/Scripts/FiniteStateMachine/Finite State Machine Editor/FSMEditor.cs
+++ Assets/Scripts/FiniteStateMachine/Finite State Machine Editor/FSMEditor.cs
@@ -839,7 +839,8 @@ namespace FiniteStateMachineEditor
 
             _parameters.Remove(editorToRemove);
             Destroy(editorToRemove.gameObject);
-            
+            AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(editorToRemove.Parameter));
+
             if (_selectedTransition != null)
             {
                 UpdateShownMinDuration();
