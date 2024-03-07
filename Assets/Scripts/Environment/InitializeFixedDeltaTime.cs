e876c7cb86efb2d94cd2572ce122494225575885
CaseyWalshWarder <111548933+CaseyWalshWarder@users.noreply.github.com>
2024-03-06 20:17:08 -0500

    Add InitializeFixedDeltaTime
    Will likely cause a merge conflict with the player rigidbody task

---
+++ Assets/Scripts/Environment/InitializeFixedDeltaTime.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Earlier in script execution order.
+public class InitializeFixedDeltaTime : MonoBehaviour
+{
+    private void Awake()
+    {
+        // If the game is running at the monitor's framerate (with vsync), do 2 physics ticks per frame.
+        // Non-integer number per frame would cause a slight jitter because the number of ticks would change
+        // each frame.
+
+        // This requires vsync enabled to avoid jitter. I'm not sure there's an advantage to not using vsync. Maybe it could
+        // feel slightly less delayed because of the frame buffer, but then we could just give settings for
+        // the frame buffer count rather than a setting for vsync. I'm not sure. We should also consider disabling
+        // vsync being better at low framerates, maybe.
+
+        // refreshRate isn't exact. Would need to update Unity to use refreshRateRatio.
+        double frameRate = Screen.currentResolution.refreshRate; 
+        double physicsRate = frameRate * (frameRate < 120 ? 2 : 1);
+        Time.fixedDeltaTime = (float)(1 / physicsRate);
+    }
+}
